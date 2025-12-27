bb257d4713e631e4f739cc0baa10ea3336cd98fd
Mikołaj Jussak <mikolajjussak@gmail.com>
2025-12-27 21:45:07 +0100

    TwoSum.cs

---
+++ TwoSum.cs
@@ -0,0 +1,22 @@
+﻿
+public class Solution
+{
+    public int[] TwoSum(int[] nums, int target)
+    {
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            for (int j = i + 1; j < nums.Length; j++)
+            {
+                if (nums[i] + nums[j] == target)
+                {
+                    int[] answ = new int[] { i, j };
+                    return answ;
+                }
+
+            }
+
+        }
+        return Array.Empty<int>();
+    }
+}

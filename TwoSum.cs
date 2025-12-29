33e79f959237873e654442e08618497b74cb0c16
Mikołaj Jussak <mikolajjussak@gmail.com>
2025-12-30 00:14:56 +0100

    Add comments to TwoSum method
    Added comments to explain the TwoSum method and its functionality.

--- TwoSum.cs
+++ TwoSum.cs
@@ -1,4 +1,9 @@
 ﻿
+//Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+//Input: nums = [2,7,11,15], target = 9
+//Output: [0,1]
+//Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+
 public class Solution
 {
     public int[] TwoSum(int[] nums, int target)
@@ -19,4 +24,5 @@ public class Solution
         }
         return Array.Empty<int>();
     }
+
 }

c403ac1e0820d7fa79dc4eff82ce09e3914dc310
Mikołaj Jussak <mikolajjussak@gmail.com>
2025-12-31 22:55:57 +0100

    Add comments for LengthOfLastWord method
    Add comments to explain the LengthOfLastWord method.

--- LengthOfLastWord.cs
+++ LengthOfLastWord.cs
@@ -1,3 +1,8 @@
+//Given a string s consisting of words and spaces, return the length of the last word in the string.
+//A word is a maximal substring consisting of non-space characters only.
+//Input: s = "Hello World"
+//Output: 5
+
 public class Solution {
     public int LengthOfLastWord(string s) {
         s= s.Trim();

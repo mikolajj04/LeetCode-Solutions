f299123436d368adeaeced8f1fd2dcb87476dc9b
Mikołaj Jussak <mikolajjussak@gmail.com>
2026-02-09 22:04:35 +0100

    Add comments for Roman numeral conversion examples

---
+++ RomantoInteger.cs
@@ -0,0 +1,12 @@
+//Given a roman numeral, convert it to an integer.
+//Example 1:
+//Input: s = "III"
+//Output: 3
+//Explanation: III = 3.
+
+//Example 2:
+//Input: s = "LVIII"
+//Output: 58
+//Explanation: L = 50, V= 5, III = 3.
+
+

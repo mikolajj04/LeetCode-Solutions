f379662c7c608540a949752a89c53686611cb676
Mikołaj Jussak <mikolajjussak@gmail.com>
2026-02-10 21:00:15 +0100

    Add RomanToInt method in Solution class
    Implemented a method to convert Roman numerals to integers.

--- RomantoInteger.cs
+++ RomantoInteger.cs
@@ -10,4 +10,29 @@
 //Explanation: L = 50, V= 5, III = 3.
 
 
-//Paste my solution!!!
+public class Solution {
+    public int RomanToInt(string s) {
+        
+            Dictionary<char, int> roman= new Dictionary<char, int>{
+
+                {'I', 1},
+                {'V' ,5},
+                {'X' ,10},
+                {'L', 50},
+                {'C',100},          
+                {'D',500},
+                {'M',1000}
+            };
+            int total = 0;
+            for (int i=0; i<s.Length; i++){
+                if(i+1<s.Length && roman[s[i]]< roman[s[i+1]]){
+                    total-=roman[s[i]];
+                }else{
+                    total+=roman[s[i]];
+                }
+            }
+            return total;
+
+
+    }
+}

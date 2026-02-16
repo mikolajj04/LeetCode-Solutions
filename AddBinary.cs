aaf5e08e59d9fe99a3feab173a7a37b5635192e3
Mikołaj Jussak <mikolajjussak@gmail.com>
2026-02-16 23:41:18 +0100

    Add method to sum two binary strings
    Implemented a method to add two binary strings and return the result as a binary string.

--- AddBinary.cs
+++ AddBinary.cs
@@ -12,4 +12,28 @@
 //Output: "10101"
  
 
-//Solution: PASTE SPOLUTION!
+public class Solution {
+    public string AddBinary(string a, string b) {
+        StringBuilder sb= new StringBuilder();
+        int i = a.Length -1;
+        int j = b.Length - 1;
+        int carry = 0;
+        while(i>= 0 || j>=0 || carry > 0){
+            int sum= carry;
+            if(i>=0){
+                sum+= a[i] - '0';
+                i--;
+            }
+            if(j>=0){
+                sum+= b[j] - '0';
+                j--;
+            }
+            sb.Append(sum%2);
+            carry = sum/2;
+        }
+
+        char[] charArray = sb.ToString().ToCharArray();
+        Array.Reverse(charArray);
+        return new string(charArray);
+    }
+}

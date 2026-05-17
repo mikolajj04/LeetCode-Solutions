f4b4200b5e26c1224918c6d8a64300374999f9e5
Mikołaj Jussak <mikolajjussak@gmail.com>
2026-05-17 22:36:49 +0200

    Optimize MissingNumber method using Gauss method
    Refactored MissingNumber method to use Gauss method for optimal performance.

--- MissingNumber.cs
+++ MissingNumber.cs
@@ -1,34 +1,37 @@
 // Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.
 
- 
-
 // Example 1:
 
 // Input: nums = [3,0,1]
-
 // Output: 2
 
 // Explanation:
-
 // n = 3 since there are 3 numbers, so all numbers are in the range [0,3]. 2 is the missing number in the range since it does not appear in nums.
 
 // Example 2:
 
 // Input: nums = [0,1]
-
 // Output: 2
 
 // Explanation:
-
 // n = 2 since there are 2 numbers, so all numbers are in the range [0,2]. 2 is the missing number in the range since it does not appear in nums.
 
+//Much more optimal solution (Gauss method):
+public class Solution {
+    public int MissingNumber(int[] nums) {
+       int ProperSum= ((nums.Length+1) * nums.Length) / 2;
+        int ActualSum= 0;
+        for(int i=0; i<nums.Length; i++){
+            ActualSum+=nums[i];
+        }
+        return ProperSum - ActualSum;
+    }
+}
 
 
 public class Solution {
     public int MissingNumber(int[] nums) {
-        int sum= ((nums.Length-1) * nums.Length)/2
-
-       for (int i = 0; i < nums.Length; i++)
+        for (int i = 0; i < nums.Length; i++)
  {
      bool hasNumber = false;
      for(int j = 0; j< nums.Length; j++)
@@ -36,7 +39,6 @@ public class Solution {
          if(i == nums[j])
          {
              hasNumber= true;
-             break;
              
          }
          
@@ -45,7 +47,9 @@ public class Solution {
 
  }
  return nums.Length;
-
-        
-    }
+    
 }
+
+
+
+ 
